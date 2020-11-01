b8f0b18e9a1528a40164020c3a8eaf18bdc64326
ilr00743 <ilr00743@gmail.com>
2020-11-01 22:12:10 +0300

    Update CustomerTest.java

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -7,6 +7,7 @@ public class CustomerTest {
     public static void main(String[] args){
         Customer customer = new Customer();
         customer.displayCustomerInfo();
+        System.out.println("\n");
         customer.setID(12);
         customer.setStatus(false);
         customer.setTotal(2020.15);
